CAF-staging/msm8916_kernel

msm: mdss: Call blank and panel OFF events only once
For command mode dual DSI panels, currently the interface blank
and panel OFF events are called twice, once for each interface
control off function. The panel and both the DSI controllers get
turned off during the first interface BLANK and PANEL_OFF events
itself since each interface handler internally gets called for
both the DSI controllers. Hence, call the interface events only
once for the left mixer controller off function.

Change-Id: Ia30dd4b728345e478e1ba6999fbbb49716e77abe
Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -567,11 +567,13 @@ int mdss_mdp_cmd_stop(struct mdss_mdp_ctl *ctl)
 	memset(ctx, 0, sizeof(*ctx));
 	ctl->priv_data = NULL;
 
-	ret = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_BLANK, NULL);
-	WARN(ret, "intf %d unblank error (%d)\n", ctl->intf_num, ret);
+	if (ctl->num == 0) {
+		ret = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_BLANK, NULL);
+		WARN(ret, "intf %d unblank error (%d)\n", ctl->intf_num, ret);
 
-	ret = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_PANEL_OFF, NULL);
-	WARN(ret, "intf %d unblank error (%d)\n", ctl->intf_num, ret);
+		ret = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_PANEL_OFF, NULL);
+		WARN(ret, "intf %d unblank error (%d)\n", ctl->intf_num, ret);
+	}
 
 	ctl->stop_fnc = NULL;
 	ctl->display_fnc = NULL;

NimitzDEV/hammerhead_kernel_caf

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -707,13 +707,15 @@ int mdss_mdp_cmd_stop(struct mdss_mdp_ctl *ctl)
 	memset(ctx, 0, sizeof(*ctx));
 	ctl->priv_data = NULL;
 
-	ret = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_BLANK, NULL);
-	WARN(ret, "intf %d blank error (%d)\n", ctl->intf_num, ret);
+	if (ctl->num == 0) {
+		ret = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_BLANK, NULL);
+		WARN(ret, "intf %d blank error (%d)\n", ctl->intf_num, ret);
 
-	ret = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_PANEL_OFF, NULL);
-	WARN(ret, "intf %d panel off error (%d)\n", ctl->intf_num, ret);
+		ret = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_PANEL_OFF, NULL);
+		WARN(ret, "intf %d panel off error (%d)\n", ctl->intf_num, ret);
 
-	mdss_mdp_cmd_tearcheck_setup(ctl, false);
+		mdss_mdp_cmd_tearcheck_setup(ctl, false);
+	}
 
 	ctl->stop_fnc = NULL;
 	ctl->display_fnc = NULL;